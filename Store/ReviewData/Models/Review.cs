a58a83a3a001630a336878e645a8eaecd534cc3d
callum <callum.kirkwood@outlook.com>
2020-01-06 11:07:02 +0000

    Rating range added

--- Store/ReviewData/Models/Review.cs
+++ Store/ReviewData/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Store.ReviewData.Models
     {
         public int Id { get; set; }
         public int ProductId { get; set; }
+        [Range(0, 5, ErrorMessage = "A rating must be between 0 and 5.")]
         public int Rating { get; set; }
         public string Comment { get; set; }
         public bool Active { get; set; }
